27f02a6b5cbbd21ad8deeefc1313ec61ed0f44f3
faga1 <1064827638@qq.com>
2022-08-09 22:55:17 +0800

    feat: 实现粘贴

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,34 @@
 import * as vscode from 'vscode';
-
+const isEqual = require("lodash.isequal");
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('pastestack.copy', () => {
+	const stack:string[] = [];
+	context.subscriptions.push(vscode.commands.registerCommand('pastestack.copy', () => {
 		const editor = vscode.window.activeTextEditor;
 		const range = editor?.selection;
-		const stack:string[] = [];
 		const content = editor?.document.getText(range);
 		content && stack.push(content);
-	});
-
-	context.subscriptions.push(disposable);
+		console.log(stack);
+		
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('pastestack.paste',() => {
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor?.selection as vscode.Selection;
+		const range = new vscode.Range(selection.start, selection.end);
+		const value = stack.pop();
+		if (!value) {
+			vscode.window.showInformationMessage('stack is empty');
+			return;
+		}
+		editor?.edit((edit) => {
+			if (isEqual(range.start,range.end)) {
+				edit.insert(range.start,value);
+				return;
+			}
+			edit.replace(range,value);
+		});
+	}));
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
